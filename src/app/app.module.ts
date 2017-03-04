fc4cfefdc4a0a310a9e7bca558447c27b8a444bd
aniruddha <aniruddha.kudalkar@gmail.com>
2017-03-04 15:09:36 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { CarComponent } from './car/car.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    CarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/app.routing.ts
@@ -0,0 +1,11 @@
+import { CarComponent } from './car/car.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
+
+import {Routes} from '@angular/router'
+
+export const PATHS : Routes = [
+    {path :'', component : DashboardComponent},
+    {path : 'user', component : UserComponent},
+    {path : 'car', component : CarComponent}    
+];
