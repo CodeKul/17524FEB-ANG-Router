a23ad58a0116145602431f79890d9da9a05ed583
aniruddha <aniruddha.kudalkar@gmail.com>
2017-03-12 14:53:06 +0530

    router : redirection and styling

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,18 +5,19 @@ import { CarComponent } from './car/car.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
 
-import {Routes, RouterModule} from '@angular/router'
+import { Routes, RouterModule } from '@angular/router'
 
-export const CAR_ROUTES : Routes = [
-    {path :'details', component : DetailsComponent},
-    {path : 'profile', component : ProfileComponent}
+export const CAR_ROUTES: Routes = [
+    { path: 'details', component: DetailsComponent },
+    { path: 'profile', component: ProfileComponent }
 ];
 
-export const PATHS : Routes = [
-    {path :'', component : DashboardComponent},
-    {path : 'user/:id', component : UserComponent},
-    {path : 'car', component : CarComponent, children : CAR_ROUTES},
-    {path : 'login', component : LoginComponent},    
+export const PATHS: Routes = [
+    { path: 'car', component: CarComponent, children: CAR_ROUTES },
+    { path: 'user', redirectTo: 'user/1' },
+    { path: 'user/:id', component: UserComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '', component: DashboardComponent }
 ];
 
 export const AppRouter = RouterModule.forRoot(PATHS);
